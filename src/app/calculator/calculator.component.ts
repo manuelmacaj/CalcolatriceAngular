d1a80f287111a8ead5a45a070507f91dbd655670
manuelmacaj <49785644+manuelmacaj@users.noreply.github.com>
2022-11-13 18:27:10 +0100

    fix calcolatrice

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,9 +10,9 @@ export class CalculatorComponent implements OnInit {
   numeriCalcolatrice = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
   simboli = ['+', '-', '*', '/'];
 
-  num1 = 0;
-  segnoSelezionato = '';
+  num1: number = 0;
   num2 = 0;
+  segnoSelezionato = '';
   display = "0";
 
   constructor() { }
@@ -28,36 +28,47 @@ export class CalculatorComponent implements OnInit {
   }
 
   selezionaSegno(segno: string) {
+    this.num1 = parseFloat(this.display);
     this.segnoSelezionato = segno;
-    this.num1 = Number(this.display.toString());
     this.display = "";
   }
 
-  inserisciNumero2(): void {
-    if (this.display != String(this.num1))
-      this.num2 = Number(this.display.toString());
+  private checkSecondNumber() {
+    if (this.num2 == 0 || this.num1 != Number(this.display))
+      this.num2 = Number(this.display)
   }
 
+
   calcolo() {
-    this.inserisciNumero2();
+    this.checkSecondNumber();
+
+    console.log(this.num1 + " " + this.num2)
+    const a = this.num1;
+    const b = this.num2;
+    let result: number
     switch (this.segnoSelezionato) {
       case '+':
-        console.log(this.num1 + " + " + this.num2);
-        this.display = String(this.num1 += this.num2)
+        result = a + b;
+        this.resultCalc(result);
         break;
       case '-':
-        console.log(this.num1 + " - " + this.num2);
-        this.display = String(this.num1 -= this.num2)
+        result = a - b;
+        this.resultCalc(result);
         break;
       case '*':
-        console.log(this.num1 + " * " + this.num2);
-        this.display = String(this.num1 *= this.num2)
+        result = a * b;
+        this.resultCalc(result);
         break;
       case '/':
-        console.log(this.num1 + " / " + this.num2);
-        this.display = String(this.num1 /= this.num2)
+        result = a / b;
+        this.resultCalc(result);
         break;
     }
+  }
+
+  private resultCalc(result: number) {
+    this.num1 = result
+    this.display = result.toString()
   }
 
   clearAll() {
